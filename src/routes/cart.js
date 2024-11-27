5484ba2fc81a9ae2560bc69fffeeda09b3c7818d
Sykrillax <88naufalfarras@gmail.com>
2024-11-28 02:59:06 +0700

    fix quantity logic and add clear all feature

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addToCart, updateCartQuantity, removeFromCart, getCart } from '../controllers/cart.js';
+import { addToCart, updateCartQuantity, removeFromCart, clearCart, getCart } from '../controllers/cart.js';
 import { safeRoute, verifyRole } from '../middlewares/middleware.js';
 
 const router = express.Router();
@@ -8,6 +8,7 @@ const router = express.Router();
 router.post('/add', safeRoute, verifyRole('buyer'), addToCart);
 router.put('/update', safeRoute, verifyRole('buyer'), updateCartQuantity);
 router.delete('/remove/:cartId', safeRoute, verifyRole('buyer'), removeFromCart);
+router.delete('/clear', safeRoute, verifyRole('buyer'), clearCart); // New route
 router.get('/', safeRoute, verifyRole('buyer'), getCart);
 
 export default router;
